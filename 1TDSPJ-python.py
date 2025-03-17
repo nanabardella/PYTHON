e35456d0c8d93dca31a2241d1abbb7cff645df8b
nanabardella <labsfiap@fiap.com.br>
2025-03-17 09:34:35 -0300

    Aula3Python

---
+++ 1TDSPJ-python.py
@@ -0,0 +1,10 @@
+print ("Hello World")
+nome = input("Digite seu nome: ")
+print(f"Olá, {nome}!")
+nome = "Giovanna"
+idade = 21
+print("Meu nome é {} e tenho {} anos.".format(nome,idade))
+
+nome = "Giovanna"
+idade = 21
+print(f"Meu nome é {nome} e tenho {idade} anos.")
---
+++ 1TDSPJexercicio-python.py
@@ -0,0 +1,7 @@
+a = 11
+b = 3
+print(a + (b*a))
+print(b*a + b)
+print(a // b)
+print(b % a)
+print(b // a)

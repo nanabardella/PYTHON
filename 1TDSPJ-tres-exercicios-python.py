1e27e958c58e8855fd4f4f6ae406111a8e02203f
nanabardella <nanabardella@gmail.com>
2025-03-19 13:41:46 -0300

    Alterações nos exercícios de python.

---
+++ 1TDSPJ-tres-exercicios-python.py
@@ -0,0 +1,13 @@
+a = 5
+b = 9
+x = not True
+y = False and True
+m = "Casa"
+n = "Mesa"
+print(x and y == x or y)
+print(a*b <= b-a)
+print(not(a == b or not b != a))
+print(not(m != n and b > a))
+print(b + a <= a * b and y)
+print(x and not y or a // b != a % b)
+
